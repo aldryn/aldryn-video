b92175bc5ab92959a725bdb3e267e0a9981fc236
marco federighi <federighi.marco@gmail.com>
2015-07-10 16:42:11 +0200

    Add tests structure

---
+++ test_settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+HELPER_SETTINGS = {
+    'ALLOWED_HOSTS': ['localhost'],
+    'CMS_LANGUAGES': {1: [{'code': 'en', 'name': 'English'}]},
+    'LANGUAGES': (('en', 'English'),),
+    'LANGUAGE_CODE': 'en',
+}
