1eabd27d283d08a167f34eb88e321276bd0a1f4f
marco federighi <m.federighi@nephila.it>
2015-07-14 14:46:30 +0200

    Update tests structure

--- test_settings.py
+++ test_settings.py
@@ -2,10 +2,30 @@
 # -*- coding: utf-8 -*-
 
 HELPER_SETTINGS = {
+    'INSTALLED_APPS': [
+        'aldryn_boilerplates',
+    ],
     'ALLOWED_HOSTS': ['localhost'],
     'CMS_LANGUAGES': {1: [{'code': 'en', 'name': 'English'}]},
     'LANGUAGES': (('en', 'English'),),
     'LANGUAGE_CODE': 'en',
+    'CMS_PERMISSION': True,
+    'TEMPLATE_CONTEXT_PROCESSORS': [
+        'aldryn_boilerplates.context_processors.boilerplate',
+    ],
+    'STATICFILES_FINDERS': [
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        # important! place right before django.contrib.staticfiles.finders.AppDirectoriesFinder
+        'aldryn_boilerplates.staticfile_finders.AppDirectoriesFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    ],
+    'TEMPLATE_LOADERS': [
+        'django.template.loaders.filesystem.Loader',
+        # important! place right before django.template.loaders.app_directories.Loader
+        'aldryn_boilerplates.template_loaders.AppDirectoriesLoader',
+        'django.template.loaders.app_directories.Loader',
+    ],
+    'ALDRYN_BOILERPLATE_NAME': 'bootstrap3',
 }
 
 def run():
